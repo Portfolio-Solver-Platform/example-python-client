541ca8d18730a371ce7abfb712b80bc11d06d07c
Jonas Bork <jonas10bork@gmail.com>
2025-10-07 16:28:40 +0200

    Added more routes

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 class Config:
     client_id: str = "users"
     client_secret: str = ""
-    scope: str = "profile email"
+    scope: str = "openid profile email"
     server_metadata_url: str = (
         "http://local/api/user/v1/.well-known/openid-configuration"
     )
--- src/main.py
+++ src/main.py
@@ -7,15 +7,26 @@ from .config import Config
 def main():
     endpoints = get_endpoints()
 
-    client = OAuth2Session(
+    session = OAuth2Session(
         client_id=Config.client_id,
         client_secret=Config.client_secret,
         scope=Config.scope,
         token_endpoint=endpoints["token_endpoint"],
     )
 
-    token = client.fetch_token(username="admin", password="admin")
-    print(token)
+    with session:
+        get_token(session)
+        user_test(session)
+
+
+def get_token(session: OAuth2Session):
+    return session.fetch_token(username="admin", password="admin")
+
+
+def user_test(session: OAuth2Session):
+    response = session.get("http://local/api/user/v1/test")
+    response.raise_for_status()
+    print(response.json())
 
 
 def get_endpoints() -> dict:
