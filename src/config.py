16aeb9d873c4ff81f658ada094462cd1c9c92ded
sofusl90 <sofus@laubel.net>
2025-11-18 13:56:52 +0100

    added examples

--- src/config.py
+++ src/config.py
@@ -1,10 +1,10 @@
 class Config:
     client_id: str = "third-party-app"
     client_secret: str = ""
-    scope: str = "solver-director:projects:read"
+    scope: str = "solver-director:projects:read solver-director:projects:write"
     server_metadata_url: str = (
         "http://local/api/user/v1/.well-known/openid-configuration"
     )
 
     class Timeout:
-        default = (2, 10)  # (connect timeout, read timeout)
+        default = (2, 24)  # (connect timeout, read timeout)
