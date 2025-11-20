d20de5859258911c10e846cbcd0c31ae7203730b
Jonas Bork <59069012+jonas-bork@users.noreply.github.com>
2025-11-20 16:00:15 +0100

    Decode token to see contents (#2)
    * Decode token
    
    * Fixed ruff warnings
    
    * Moved joserfc from dev dependencies to dependencies
    
    * Added timeouts

--- src/main.py
+++ src/main.py
@@ -1,41 +1,18 @@
-from device_auth import DeviceAuth
+from auth import Auth
+from auth.device_auth import DeviceAuth
 from config import Config
-import requests
-from pprint import pprint
 
 
 def main():
+    auth = Auth(Config)
     access_token, refresh_token = DeviceAuth(Config).get_token()
     print("==== Access token ====")
     print(access_token)
+    print("==== Decoded access token claims ====")
+    print(auth.decode_token(access_token).claims)
     print()
     print("==== Refresh token ====")
     print(refresh_token)
-
-    # groups(access_token)
-    projects(access_token)
-
-
-def groups(token: str):
-    response = requests.get(
-        "http://local/api/solverdirector/v1/groups",
-        headers={"Authorization": f"Bearer {token}"},
-        timeout=Config.Timeout.default,
-    )
-    pprint(response)
-    response.raise_for_status()
-    pprint(response.json())
-
-
-def projects(token: str):
-    response = requests.get(
-        "http://local/api/solverdirector/v1/projects",
-        headers={"Authorization": f"Bearer {token}"},
-        timeout=Config.Timeout.default,
-    )
-    pprint(response)
-    response.raise_for_status()
-    pprint(response.json())
 
 
 if __name__ == "__main__":
